a07696d8695929feaafa3fca1178f576bdbb0080
oak-23 <oaksoeoo23@gmail.com>
2026-01-04 16:30:02 +0800

    Update barometer_test.ino

--- barometer_test.ino
+++ barometer_test.ino
@@ -1,30 +1,36 @@
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
-#include <WiFi.h>
 
-// Connect Arduino to WiFi
-const char* ssid = "YOUR_SSID"; // change this
-const char* password = "YOUR_PASSWORD"; // change this
+#include <WiFiS3.h>
 
-// modify server name
-const char* serverUrl = "http://cops/endpoint"; // Replace with your server details
+char ssid[] = "Airtel_suma_8138";
+char pass[] = "air24004";
+int status = WL_IDLE_STATUS;
+
+const char* server = "cops.snehashish.tech";
+int port = 8080;
+const char* path = "/sensor/quantity";
+
+WiFiClient client;
+
 
 void setup() {
+  // put your setup code here, to run once:
   pinMode(2, INPUT);   // Connect HX710 OUT to Arduino pin 2
   pinMode(3, OUTPUT);  // Connect HX710 SCK to Arduino pin 3
+  pinMode(4, INPUT);   // Connect second HX710 OUT to Arduino pin 2
+  pinMode(5, OUTPUT);  // Connect second HX710 SCK to Arduino pin 3
+
+
   Serial.begin(9600);
-
-  WiFi.mode(WIFI_STA); // Use WIFI_STA for client mode, WIFI_AP for Access Point, WIFI_AP_STA for both
-
-  // Start the Wi-Fi connection
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
+  // IMPORTANT: remove while (!Serial) on non‑native USB boards
+  delay(1000); // give Serial time to come up
+  Serial.println("Booting...");
 
   // Connect to WiFi
-  while (WiFi.status() != WL_CONNECTED) {
+  while (status != WL_CONNECTED) {
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
     delay(1000);
-    Serial.print("."); // Print dots to show progress
   }
 
   Serial.println("Connected to WiFi");
@@ -33,9 +39,21 @@ void setup() {
 }
 
 void loop() {
+  float pressureResult1 = readPressure1();
+  float pressureResult2 = readPressure2();
 
-  // Reading pressure
-  while (digitalRead(2)) {} // wait for the current reading to finish
+  Serial.print("Pressure: ");
+  Serial.println(pressureResult, 6);
+
+  sendPressure(pressureResult1, pressureResult2);
+
+  delay(5000);  // send every 5 seconds
+}
+
+float readPressure1() {
+  char formattedValue[8];
+  // wait for the current reading to finish
+  while (digitalRead(2)) {}
 
   // read 24 bits
   long result = 0;
@@ -48,7 +66,7 @@ void loop() {
     }
   }
 
-  // get the 2s compliment
+  // get the 2's complement
   result = result ^ 0x800000;
 
   // pulse the clock line 3 times to start the next pressure reading
@@ -56,46 +74,81 @@ void loop() {
     digitalWrite(3, HIGH);
     digitalWrite(3, LOW);
   }
-
+  
   float pressure = (result /100000000.0)/14.504 ;
   
-  // display pressure
-  Serial.println(pressure);
+  float initial_pressure = 100; // change this 
+  float water_exerted_pressure = 200; // change this 
+  float total_pressure = water_exerted_pressure - initial_pressure; // calculate total volume in terms of pressure
+  float percentage_left = 100 - (((total_pressure - (pressure - initial_pressure)) / total_pressure) * 100); 
 
-  // Calculate Percentage
-    float initial_pressure = 100; // change this 
-    float water_exerted_pressure = 200; // change this 
-    float total_pressure = water_exerted_pressure - initial_pressure; // calculate total volume in terms of pressure
-    float percentage_left = 100 - (((total_pressure - (pressure - initial_pressure)) / total_pressure) * 100); 
+  return pressure; // Return the result
+}
 
-  // for sending to server
-  if (status == WL_CONNECTED) {
-    // declare http object
-    HTTPClinet http;
-    http.begin(server); // Specify your server address and endpoint
-    http.addHeader("Content-Type", "application/json"); // Important header
+float readPressure2() {
+  char formattedValue[8];
+  // wait for the current reading to finish
+  while (digitalRead(4)) {}
 
-    // Initialise Json file
-    DynamicJsonDocument doc(200);
-    doc["pressure"] = pressure;
-    doc["percentage_left"] = percentage_left;
-    String requestBody;
-    serializeJsonPretty(doc, requestBody);
-  }
-    // Send post request
-    int httpResponseCode = http.POST(requestBody);
-
-    // Testing statements (debugging)
-    if (httpResponseCode > 0) {
-      Serial.printf("Posted Json:c", httpResponseCode);
-      String response = http.getString();
-      Serial.println(response);
-    } else {
-      Serial.printf("Error: ", http.errorToString(httpResponseCode).c_str());
+  // read 24 bits
+  long result = 0;
+  for (int i = 0; i < 24; i++) {
+    digitalWrite(5, HIGH);
+    digitalWrite(5, LOW);
+    result = result << 1;
+    if (digitalRead(4)) {
+      result++;
     }
-
-    http.end();
   }
 
-  delay(5000); // Post every 5 seconds
+  // get the 2's complement
+  result = result ^ 0x800000;
+
+  // pulse the clock line 3 times to start the next pressure reading
+  for (char i = 0; i < 3; i++) {
+    digitalWrite(5, HIGH);
+    digitalWrite(5, LOW);
+  }
+  
+  float pressure = (result /100000000.0)/14.504 ;
+  
+  float initial_pressure = 100; // change this 
+  float water_exerted_pressure = 200; // change this 
+  float total_pressure = water_exerted_pressure - initial_pressure; // calculate total volume in terms of pressure
+  float percentage_left = 100 - (((total_pressure - (pressure - initial_pressure)) / total_pressure) * 100); 
+
+  return pressure; // Return the result
+}
+
+void sendPressure(float pressure1, float pressure2) {
+  if (!client.connect(server, port)) {
+    Serial.println("Connection failed");
+    return;
+  }
+
+  // Build JSON body
+  String jsonBody = "{ \"pressure\": " + String(pressure1, 6) + "," + "\"pressure2\": " + String(pressure2, 6) + " }";
+
+  // HTTP POST request
+  client.print(String("POST ") + path + " HTTP/1.1\r\n");
+  client.print(String("Host: ") + server + "\r\n");
+  client.println("Content-Type: application/json");
+  client.print("Content-Length: ");
+  client.println(jsonBody.length());
+  client.println("Connection: close");
+  client.println();
+  client.println(jsonBody);
+
+  Serial.println("Sent:");
+  Serial.println(jsonBody);
+
+  // Read response (optional but recommended)
+  while (client.connected()) {
+    if (client.available()) {
+      String line = client.readStringUntil('\n');
+      Serial.println(line);
+    }
+  }
+
+  client.stop();
 }
